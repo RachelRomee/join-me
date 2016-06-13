d053c18934a85cebcbb4e0e174966c3853064654
RachelRomee <rachelromee@icloud.com>
2016-06-13 16:03:21 +0200

    Created devices and fixed password and email columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  belongs_to :listings
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  belongs_to :listing
 end
