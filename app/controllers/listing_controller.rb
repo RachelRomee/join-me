e5203ee8f4b03ef6332e1354c62818d85dfb5928
RachelRomee <rachelromee@icloud.com>
2016-06-15 16:08:24 +0200

    Work on styling - navbar, container, listings view

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,5 +1,32 @@
 class ListingController < ApplicationController
 
   def index
+    @listings = Listing.all
   end
+
+  def new
+    @listing = Listing.new
+    @listing.user_id = params[:user_id]
+  end
+
+  def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+# EXAMPLE FROM AUTHOR PROJECT
+#   def novel_params
+#   params.require( :novel ).permit( :title, :year, :cover, :plot, :author_id, genre_ids: [] )
+# end
+
 end
