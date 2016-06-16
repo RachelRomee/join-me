4949a56b0a68b884776b2348f615c13491dbc383
RamonSchmitt <ramon.schmitt@yahoo.com>
2016-06-16 13:28:11 +0200

    added more views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,4 +4,12 @@ class UserController < ApplicationController
     @user = User.new
   end
 
+  def show
+     @user = User.find( params[:id] )
+  end
+
+  def edit
+     @user = User.find( params[:id] )
+  end
+
 end
