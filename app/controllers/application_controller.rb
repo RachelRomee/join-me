99b5ffef2002bf48fceb6c26a0fe579ea6e231dc
RachelRomee <rachelromee@icloud.com>
2016-06-17 13:52:52 +0200

    Route for redirect after login to listings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, if: :needs_sign_in?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    listings_path
+  end
+
   protected
 
     def needs_sign_in?
