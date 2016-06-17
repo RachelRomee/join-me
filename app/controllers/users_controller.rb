eedf87accb1f42e6a0adde774b849a1e862b3537
RachelRomee <rachelromee@icloud.com>
2016-06-17 11:56:14 +0200

    Get listings on user profile and fix user profile link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
 
+
+
   def show
     @user = User.find(params[:id])
+
+    #@listing = Listing.find(params[:id])
+    @listings = Listing.where(user: @user).order(created_at: :desc)
   end
 
   def listing
     #@user = User.find(params[:id])
     #authorize! :read, @user
-
-    @listings = Listing.where(listing_id: @user).order(created_at: :desc)
-    authorize! :read, @posts
+    @listing = Listing.all
+    @listings = Listing.where(user: @user).order(created_at: :desc)
+    authorize! :read, @listings
 
     # @bookings = @user.likes.joins(:post).order("posts.created_at DESC")
     # authorize! :read, @likes
